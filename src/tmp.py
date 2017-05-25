1e4627047a4c10c2659a6983845744c2c5e04e05
chucker34 <kawakami34@gmail.com>
2017-05-25 21:47:17 +0900

    3.2.4

--- src/tmp.py
+++ src/tmp.py
@@ -4,13 +4,13 @@ mpl.use('Agg')
 import matplotlib.pyplot as plt
 
 
-def step_function(x):
-    return np.array(x > 0, dtype=np.int)
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
 
 
 x = np.arange(-5.0, 5.0, 0.1)
-y = step_function(x)
+y = sigmoid(x)
 plt.plot(x, y)
 plt.ylim(-0.1, 1.1)
 
-plt.savefig('step')
+plt.savefig('sigmoid')
