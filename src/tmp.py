788253a44202e7d30805199e0c41dde80b7be940
chucker34 <kawakami34@gmail.com>
2017-05-25 21:39:51 +0900

    3.2.3

--- src/tmp.py
+++ src/tmp.py
@@ -1,6 +1,16 @@
 import numpy as np
+import matplotlib as mpl
+mpl.use('Agg')
+import matplotlib.pyplot as plt
 
-x = np.array([-1.0, 1.0, 2.0])
-y = x > 0
-y = y.astype(np.int)
-print(y)
+
+def step_function(x):
+    return np.array(x > 0, dtype=np.int)
+
+
+x = np.arange(-5.0, 5.0, 0.1)
+y = step_function(x)
+plt.plot(x, y)
+plt.ylim(-0.1, 1.1)
+
+plt.savefig('step')
